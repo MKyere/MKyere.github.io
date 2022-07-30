ea699794098b8d30ad2a7b4d5f1ffe4732123007
MKyere <107903151+MKyere@users.noreply.github.com>
2022-07-30 22:42:39 +0100

    reorganise repos to support github pages properly

---
+++ js/sript-tribute.js
@@ -0,0 +1,5 @@
+document.getElementById("date").innerHTML = Date();
+
+function myFunction() { 
+      document.getElementById("demo").innerHTML = "Nkrumah is played by Danny Sapani in the Netflix television series The Crown";
+}
